fc28a8dcd38204d730d1d7b907f21fefa2eb9961
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-06 13:04:43 +0530

    added user service methods

--- src/main/java/com/blog/api/services/UserService.java
+++ src/main/java/com/blog/api/services/UserService.java
@@ -1,5 +1,19 @@
 package com.blog.api.services;
 
 
+import com.blog.api.payloads.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+
+    UserDto createUser(UserDto user);
+
+    UserDto updateUserByUserId(UserDto user, Long userId);
+
+    UserDto getUserByUserId(Long userId);
+
+    List<UserDto> getAllUsers();
+
+    void deleteUserByUserId(Long userId);
 }
