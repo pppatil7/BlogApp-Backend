1579b5f8531db989fe3068d4e92962f90ef475d6
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-15 18:25:30 +0530

    img serve

--- src/main/java/com/blog/api/controllers/PostController.java
+++ src/main/java/com/blog/api/controllers/PostController.java
@@ -5,14 +5,18 @@ import com.blog.api.payloads.PostDto;
 import com.blog.api.payloads.PostResponse;
 import com.blog.api.services.FileService;
 import com.blog.api.services.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -98,6 +102,14 @@ public class PostController {
         postDto.setPostImageName(fileName);
         PostDto updatedPost = this.postService.updatePostByPostId(postDto, postId);
         return ResponseEntity.ok(updatedPost);
+    }
+
+    //method to serve files
+    @GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable String imageName, HttpServletResponse response) throws IOException {
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 
 
